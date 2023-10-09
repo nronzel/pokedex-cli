c3be5b3682bcf09d210651f808c7246181607ea0
nronzel <86695181+nronzel@users.noreply.github.com>
2023-10-09 17:34:08 -0400

    refactor: make use of client for pokeapi

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"time"
+
+	"github.com/nronzel/pokedex-cli/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
