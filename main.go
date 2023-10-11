ce36b852f6a0ecd5fea2220e7b683d7a3d0524ae
nronzel <86695181+nronzel@users.noreply.github.com>
2023-10-11 15:18:43 -0400

    chore: initialized caughtPokemon map

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
 	cfg := &config{
+		caughtPokemon: map[string]pokeapi.Pokemon{},
 		pokeapiClient: pokeClient,
 	}
 
